0cee7c73a8ccc30037ba208cd94d117cabf55173
Guilherme Felix <f3lixguilherme@gmail.com>
2025-08-08 18:29:49 -0300

    feat(auth): flask login

--- 4oModulo-FlaskAuth/app.py
+++ 4oModulo-FlaskAuth/app.py
@@ -1,17 +1,74 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from database import db
 from models.user import User
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "uau"
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
+login_manager = LoginManager()
+
+
+
 db.init_app(app)
+login_manager.init_app(app)
+
+# view é obrigatorio, é a rota que vai fazer a autenticacao
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(user_id)
 
 
-@app.route("/")
-def hello():
-  return "Teste"
+@app.route("/login", methods=['POST'])
+def login():
+  data = request.json
+  username = data.get('username')
+  password = data.get('password')
 
+
+  if username and password:
+    user = User.query.filter_by(username=username).first()
+
+    if user and user.password == password:
+      login_user(user)
+      print(current_user.is_authenticated)
+      return jsonify({
+    "mensagem":"Login realizado"
+  })
+
+
+  return jsonify({
+    "mensagem":"Credenciais inválidas"
+  }), 400
+
+
+@login_required # tipo um PreAuthorization no spring
+@app.route("/logout", method=['GET'])
+def logout():
+  logout_user()
+  return jsonify({
+    "mensagem":"Logout realizado com sucesso"
+  })
+
+@app.route("/new", methods=['POST'])
+def create_user():
+  data = request.json
+  username = data.get('username')
+  password = data.get('password')
+
+  if username and password:
+     user = User(username=username, password=password)
+     db.session.add(user)
+     db.session.commit()
+     return jsonify({
+    "mensagem":"Cadastro realizado com sucesso"
+  })
+  return jsonify({
+    "mensagem":"Credenciais inválidas"
+  }), 400
 if __name__ == "__main__":
   app.run(debug=True)
   
